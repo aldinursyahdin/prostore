96f56eea5b761a729532f03f599d7ded33399d65
aldinursyahdin <aldinursyahdinhalim@gmail.com>
2026-03-01 18:01:20 +0700

    finish auth sign in and up implementation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { ZodError } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,4 +14,37 @@ export function convertToPlainObject<T>(value: T): T {
 export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split('.');
   return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+}
+
+// Format errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+  if (error.name === 'ZodError') {
+    // Handle Zod error
+    const fieldErrors = Object.keys(error.issues).map(
+      (field) => error.issues[field].message,
+    );
+
+    return fieldErrors.join('. ');
+  } else if (
+    error.name === 'PrismaClientKnownRequestError' &&
+    error.code === 'P2002'
+  ) {
+    const fields = error.meta?.driverAdapterError?.cause?.constraint?.fields as
+      | string[]
+      | undefined;
+
+    if (fields?.length) {
+      const formattedFields = fields
+        .map((field) => field.charAt(0).toUpperCase() + field.slice(1))
+        .join(', ');
+
+      return `${formattedFields} already exists`;
+    }
+  } else {
+    // Handle other errors
+    return typeof error.message === 'string'
+      ? error.message
+      : JSON.stringify(error.message);
+  }
 }
